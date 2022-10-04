99d6c1defd5e34f5373cba65200ddf1c47d84bc1
DamaMichaelYohanna <get2dama11@gmail.com>
2022-10-04 11:01:58 +0100

    updated complain.html and views

--- administrator/views.py
+++ administrator/views.py
@@ -2,13 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-from main.models import Complain
+from main.models import Complain, ComplainTrack
 
 
 @login_required
 def all_complains(request):
     complain = Complain.objects.all()
-    context = {"complain": complain}
+    status = ComplainTrack.objects.filter(complain__in=complain)
+    context = {"complain": complain, 'status': status}
     return render(request, "administrator/all_complains.html", context)
 
 
